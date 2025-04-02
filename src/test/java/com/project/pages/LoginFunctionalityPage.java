e7fdace9a724767805fcdd3e507471179cec7f89
dianaBn <diana.balanuta.work@gmail.com>
2025-04-01 20:10:59 -0400

    Added WebElements in LoginFunctionalityPage.java

--- src/test/java/com/project/pages/LoginFunctionalityPage.java
+++ src/test/java/com/project/pages/LoginFunctionalityPage.java
@@ -1,4 +1,26 @@
 package com.project.pages;
 
+import com.project.utils.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class LoginFunctionalityPage {
+
+    public LoginFunctionalityPage(){
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    @FindBy(id = "user-name")
+    public WebElement username;
+
+    @FindBy(id = "password")
+    public WebElement password;
+
+    @FindBy(id = "login-button")
+    public WebElement loginButton;
+
+    @FindBy(xpath = "//div/h3[.='Epic sadface: Password is required']")
+    public WebElement errorMessage;
+
 }
