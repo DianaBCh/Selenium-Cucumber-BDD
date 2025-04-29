719c67df533bc4d432f8c00a15551c90a86b8f72
dianaBn <diana.balanuta.work@gmail.com>
2025-04-29 11:08:20 -0400

    Add StepDefs implementation for logout and validation steps

--- src/test/java/com/project/stepDefs/LogoutStepDefs.java
+++ src/test/java/com/project/stepDefs/LogoutStepDefs.java
@@ -2,6 +2,7 @@ package com.project.stepDefs;
 
 import com.project.pages.LoginFunctionalityPage;
 import com.project.pages.ProductFunctionalityPage;
+import com.project.utils.BrowserUtils;
 import com.project.utils.Driver;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -18,6 +19,8 @@ public class LogoutStepDefs {
     public void the_user_clicks_on_the_burger_menu_icon() {
 
         productFunctionalityPage.burgerMenuButton.click();
+        //BrowserUtils.sleep(3);//TEMP sleep used only for Chrome password popup issue
+
 
     }
     @Then("the Logout button should be visible in the navigation menu")
@@ -31,6 +34,8 @@ public class LogoutStepDefs {
     public void the_user_clicks_on_the_logout_button() {
 
         productFunctionalityPage.logoutButton.click();
+        //BrowserUtils.sleep(3);// TEMP sleep used only for Chrome password popup issue
+
 
     }
     @Then("the user should be redirected to the login page")
@@ -38,6 +43,8 @@ public class LogoutStepDefs {
 
         String currentUrl = Driver.getDriver().getCurrentUrl();
         Assert.assertEquals("User is not on the login page!","https://www.saucedemo.com/", currentUrl);
+        //BrowserUtils.sleep(3);// TEMP sleep used only for Chrome password popup issue
+
 
     }
 
@@ -45,12 +52,16 @@ public class LogoutStepDefs {
     public void the_user_attempts_to_navigate_back_using_the_browser_back_button() {
 
         Driver.getDriver().navigate().back();  // Simulate browser back button
+        //BrowserUtils.sleep(3);// TEMP sleep used only for Chrome password popup issue
+
 
     }
     @Then("the user should remain on the login page")
     public void the_user_should_remain_on_the_login_page() {
 
         Assert.assertEquals("User is not on the login page after pressing back!","https://www.saucedemo.com/", Driver.getDriver().getCurrentUrl());
+        //BrowserUtils.sleep(3);// TEMP sleep used only for Chrome password popup issue
+
 
     }
     @Then("the Products page should not be accessible")
@@ -66,12 +77,15 @@ public class LogoutStepDefs {
     public void the_user_navigates_directly_to_via_url(String pageURL) {
 
         Driver.getDriver().get(baseURL+pageURL);
+        //BrowserUtils.sleep(3);// TEMP sleep used only for Chrome password popup issue
 
     }
     @Then("an error message should be displayed: {string}")
     public void an_error_message_should_be_displayed(String expectedErrorMessage) {
 
         String actualErrorMessage = loginFunctionalityPage.errorMessage.getText();
+        //BrowserUtils.sleep(3);// TEMP sleep used only for Chrome password popup issue
+
 
         Assert.assertEquals("Error message mismatch", expectedErrorMessage, actualErrorMessage);
 
