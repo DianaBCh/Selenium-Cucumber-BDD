7cb20970f61b09a0ddf0826868570ddbbbe11963
dianaBn <diana.balanuta.work@gmail.com>
2025-04-29 13:45:30 -0400

    Generate step definitions for End-to-End flow

---
+++ src/test/java/com/project/stepDefs/EndToEndFlowStepDefs.java
@@ -0,0 +1,49 @@
+package com.project.stepDefs;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class EndToEndFlowStepDefs {
+
+    @Given("the user is on the login page")
+    public void the_user_is_on_the_login_page() {
+
+    }
+
+    @When("the user logs in with valid credentials")
+    public void the_user_logs_in_with_valid_credentials() {
+
+    }
+
+    @When("the user adds a product to the cart")
+    public void the_user_adds_a_product_to_the_cart() {
+
+    }
+
+    @When("the user proceeds to checkout")
+    public void the_user_proceeds_to_checkout() {
+
+    }
+
+    @When("the user provides valid checkout information")
+    public void the_user_provides_valid_checkout_information() {
+
+    }
+
+    @When("the user completes the purchase")
+    public void the_user_completes_the_purchase() {
+
+    }
+
+    @When("the user logs out")
+    public void the_user_logs_out() {
+
+    }
+
+    @Then("the Products page should not be accessible via browser back button")
+    public void the_products_page_should_not_be_accessible_via_browser_back_button() {
+
+
+    }
+}
